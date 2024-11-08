c47fed539950c1b4b79046781d88695e939ece96
Kevin Wu <KWu@lcs.on.ca>
2024-11-08 08:52:09 -0500

    Initial Commit

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Yixuan Wu on 2024-11-08.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
